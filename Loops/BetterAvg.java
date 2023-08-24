08a897666a7d9e5ab0f50a30c0319f0c811b9d41
SpectraAlpha <119772400+SpectraAlpha@users.noreply.github.com>
2023-08-24 16:38:32 +0530

    Optimized version of the infinite loop for avg
    For new users to work with

---
+++ Loops/BetterAvg.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void Avg(){
+        Scanner In = new Scanner(System.in);
+        int tot=0,count=-1,n=0;
+        //count set to -1 because the first count++ will set the count to 1 from 0
+        //The first run is not the first time a number is added to the total though
+        //This code was created to remove repeition of the input line n = In.nextInt() before starting a loop
+
+        do {
+
+            tot+=n;
+            System.out.print("Enter Number:");
+            n = In.nextInt();
+            count++;
+        }while(n>0);
+
+        System.out.println("Average is " + tot/count);
+
+        In.close();
+    }
+
+    public static void main(String args[]){
+
+        Avg();
+
+    }
+}
